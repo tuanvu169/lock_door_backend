865367c1fe03e6a469927dedf7eb415694315dff
tuanvu169 <tuanvuduc1609@gmail.com>
2026-01-12 21:04:32 +0700

    Tắt hash password, lưu plain text để test

--- models/User.js
+++ models/User.js
@@ -8,10 +8,10 @@ const userSchema = new mongoose.Schema({
 }, {
   collection: 'account'  // <-- Quan trọng: chỉ định tên collection chính xác
 });
-
+/*
 userSchema.pre('save', async function () {
   if (!this.isModified('password')) return;
   this.password = await bcrypt.hash(this.password, 10);
 });
-
+*/
 module.exports = mongoose.model('User', userSchema);  // Tên model vẫn là 'User' (không ảnh hưởng)
