561210fe0a4c0fdf049e9fdbd77c19ffa64ffb61
tuanvu169 <tuanvuduc1609@gmail.com>
2026-01-14 17:02:26 +0700

    Bỏ phân quyền role, login chỉ cần email + password

--- models/User.js
+++ models/User.js
@@ -1,17 +1,9 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true, trim: true },
-  password: { type: String, required: true },
-  role: { type: String, required: true, enum: ['ADMIN', 'USER'] }
-}, {
-  collection: 'account'  // <-- Quan trọng: chỉ định tên collection chính xác
+  name: String,
+  email: { type: String, unique: true, required: true },
+  passwordHash: { type: String, required: true },
 });
-/*
-userSchema.pre('save', async function () {
-  if (!this.isModified('password')) return;
-  this.password = await bcrypt.hash(this.password, 10);
-});
-*/
-module.exports = mongoose.model('User', userSchema);  // Tên model vẫn là 'User' (không ảnh hưởng)
+
+module.exports = mongoose.model("User", userSchema);
